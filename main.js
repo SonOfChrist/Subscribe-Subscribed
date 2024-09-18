db5cf515bb7c86f77df1a471773024512416f282
SoftwareDev Stan <mochogestanley80@gmail.com>
2024-09-18 12:20:48 +0300

    Subscribe_Subscribed

---
+++ main.js
@@ -0,0 +1,11 @@
+function subscribe() {
+    const buttonElement = document.querySelector('.js-subscribe-button');
+
+    if (buttonElement.innerText === 'Subscribe') {
+        buttonElement.innerHTML = 'Subscribed';
+        buttonElement.classList.add('is-subscribed');
+    } else {
+        buttonElement.innerHTML = 'Subscribe';
+        buttonElement.classList.remove('is-subscribed');
+    }
+}
